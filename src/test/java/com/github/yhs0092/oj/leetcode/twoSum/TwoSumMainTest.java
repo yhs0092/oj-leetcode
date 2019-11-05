425a71734474ad7a9f7d1132312aba7750e81190
yhs0092 <yhs0092@163.com>
2019-11-05 13:35:23 +0800

    TwoSum: fix UT
    there are more than 1 correct answers in the old cases

--- src/test/java/com/github/yhs0092/oj/leetcode/twoSum/TwoSumMainTest.java
+++ src/test/java/com/github/yhs0092/oj/leetcode/twoSum/TwoSumMainTest.java
@@ -12,8 +12,8 @@ public class TwoSumMainTest {
     TwoSumMain twoSumMain = new TwoSumMain();
     verify(twoSumMain.twoSum(new int[] {1, 2, 3, 4, 5}, 4), 0, 2);
     verify(twoSumMain.twoSum(new int[] {0, 1, 2, 4, 5, 6}, 4), 0, 3);
-    verify(twoSumMain.twoSum(new int[] {0, 1, 2, 4, 5, 6}, 7), 1, 5);
-    verify(twoSumMain.twoSum(new int[] {6, 5, 4, 3, 2, 1, 0}, 7), 0, 5);
+    verify(twoSumMain.twoSum(new int[] {0, 1, 2, 4, -1, 6}, 7), 1, 5);
+    verify(twoSumMain.twoSum(new int[] {6, 5, 4, -2, -1, 1, 0}, 7), 0, 5);
     verify(twoSumMain.twoSum(new int[] {0, 1, 2, 2, 5, 6}, 4), 2, 3);
     verify(twoSumMain.twoSum(new int[] {-1, -2, -3, -4, -5}, -8), 2, 4);
     verify(twoSumMain.twoSum(new int[] {-1, -2, 3, -4, -5}, 1), 1, 2);
