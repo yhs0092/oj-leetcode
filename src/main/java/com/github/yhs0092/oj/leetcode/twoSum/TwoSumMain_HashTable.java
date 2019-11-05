193355d0ee9ab7904bb752b1cc98fe511a77fc41
yhs0092 <yhs0092@163.com>
2019-11-05 13:35:53 +0800

    TwoSum: add HashTable solution

---
+++ src/main/java/com/github/yhs0092/oj/leetcode/twoSum/TwoSumMain_HashTable.java
@@ -0,0 +1,25 @@
+package com.github.yhs0092.oj.leetcode.twoSum;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class TwoSumMain_HashTable {
+  public int[] twoSum(int[] nums, int target) {
+    final int[] result = new int[2];
+    // num:index_of_num
+    final Map<Integer, Integer> map = new HashMap<>();
+    for (int i = 0; i < nums.length; ++i) {
+      int num = nums[i];
+      int toFind = target - num;
+      Integer toFindIndex = map.get(toFind);
+      if (null != toFindIndex) {
+        result[0] = toFindIndex;
+        result[1] = i;
+        break;
+      }
+      map.put(num, i);
+    }
+
+    return result;
+  }
+}
